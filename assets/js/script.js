1832aae8537b5d5d30bee9867a13358987664876
Victor Martins <vitor_jhoul@hotmail.com>
2022-04-20 14:07:01 -0300

    adicionando javaScript

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let nome = document.querySelector('#nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+
+
+
+function validaNome() {
+  
+    if(nome.value.lenght < 3){
+        alert('teste')
+    }
+}
+
